2e46d7fc2a9c248cb4f01dac9e23ac5b6ec2d0d2
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2024-01-08 13:14:27 -0800

    Add files via upload

---
+++ OddOneOut/OddOneOut/OddOneOutApp.swift
@@ -0,0 +1,19 @@
+//
+//  OddOneOutApp.swift
+//  OddOneOut
+//
+//  Created by Brandon Johns on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct OddOneOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.hiddenTitleBar)
+        .windowResizability(.contentSize)
+    }
+}
